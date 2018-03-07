c401cebd65df5799eb87a6512a5ae940e1986e26
huoyinghui <huoyinghui@apkpure.com>
2018-03-07 17:39:55 +0800

    add: bp 使用静态文件

---
+++ apiser/02/src/config/__init__.py
@@ -0,0 +1,23 @@
+import os
+
+def load_config():
+    """
+    Load a config class
+    :return:
+    """
+    mode = os.environ.get('MODE', 'DEV')
+    try:
+        if mode == 'PRO':
+            from .pro_config import ProConfig
+            return ProConfig
+        elif mode == 'DEV':
+            from .dev_config import DevConfig
+            return DevConfig
+        else:
+            from .dev_config import DevConfig
+            return DevConfig
+    except ImportError:
+        from .config import Config
+        return Config
+
+CONFIG = load_config()
---
+++ apiser/02/src/config/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    """
+    Basic config for proj
+    """
+    TIMEZONE = 'Asia/Shanghai'
+    BASE_DIR = os.path.dirname(os.path.abspath(__name__))

90e13e57075d54559212f668d611955595ebd976
huoyinghui <huoyinghui@apkpure.com>
2018-03-14 16:17:02 +0800

    add: mail

--- 05/src/router.js
+++ 05/src/router.js
@@ -1,15 +1,16 @@
 import Index from './views/index.vue';
-import Home from './views/home.vue';
 import Admin from './views/admin.vue';
+import Home from './views/home.vue';
+import Mail from './views/mail.vue';
 import Login from './views/login.vue';
 import User from './views/user.vue';
 import UserProfile from './views/user_profile.vue';
-
+import UserProfileDef from './views/user_profile_def.vue';
 const routers = [
     {
         path: '/',
         meta: {
-            title: ''
+            title: 'index'
         },
         component: Index,
     },
@@ -27,18 +28,35 @@ const routers = [
         },
         component: Admin,
     },
+    // {
+    //     path: '/user/:id',
+    //     meta: {
+    //         title: 'user'
+    //     },
+    //     component: User,
+    //     children: [
+    //         {
+    //             path: '',
+    //             component: UserProfileDef
+    //         },
+    //         {
+    //             path: 'profile',
+    //             component: UserProfile,
+    //             children: [
+    //                 {
+    //                     path: 'mail',
+    //                     component: Mail,
+    //                 }
+    //             ]
+    //         }
+    //     ]
+    // },
     {
-        path: '/user/:id',
+        path:'/user/:id/profile/mail',
         meta: {
-            title: 'user'
+            title: 'mail'
         },
-        component: User,
-        children: [
-            {
-                path: 'profile',
-                component: UserProfile
-            }
-        ]
+        component: Mail,
     },
     {
         path: '/login',

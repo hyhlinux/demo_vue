292344e8ae338e30bea333b369d0de1452eab00d
huoyinghui <huoyinghui@apkpure.com>
2018-03-29 18:10:28 +0800

    backup

---
+++ 07CURD/dj-message/src/mysite/msg/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+import datetime
+
+# Create your models here.
+class Message(models.Model):
+    fromUserId = models.CharField(max_length=60)
+    toUserId = models.CharField(max_length=60)
+    status = models.IntegerField(default=0)
+    title = models.CharField(max_length=100)
+    Text = models.CharField(max_length=1000)
+    isDelete = models.BooleanField(default=False)
+    createAt = models.DateTimeField(auto_now_add=True)
+    updateAt = models.DateTimeField(default=datetime.datetime.now)
